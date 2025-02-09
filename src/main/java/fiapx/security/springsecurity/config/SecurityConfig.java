b45c8368af6f4f43123d841f1178585909e81cb2
igusvs <igoralves159@gmail.com>
2025-02-09 03:33:40 -0300

    CI CD JACOCO SONAR E ajustes infra k8s

--- src/main/java/fiapx/security/springsecurity/config/SecurityConfig.java
+++ src/main/java/fiapx/security/springsecurity/config/SecurityConfig.java
@@ -41,6 +41,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/actuator/health/liveness").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/actuator/health/readiness").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/actuator/health").permitAll()
                         .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
